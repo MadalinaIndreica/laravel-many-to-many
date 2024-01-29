b6b5ea8c517013a530c008c3d6c9b0b0d7536603
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-01-29 15:06:44 +0100

    Add: migration model and seeder for technology

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $technologies = ['laravel','js','vue.js','bootstrap','php'];
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology);
+            $newTechnology->hex_color = $faker->hexColor();
+            $newTechnology->save();
+        }
+    }
+}
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,6 +8,13 @@
            tipologia: {{ $project->type ? $project->type->name : 'nessuna tipologia' }}
             </p>
         </div>
+
+        <div class="my-3">
+            @foreach ($project->technologies as $technology)
+            <span class="badge" style="background-color: {{ $technology->hex_color }}">{{ $technology->name }}</span>
+            @endforeach
+
+        </div>
         
         <div class="mt-4">
             Data: {{ $project->created_at }}
